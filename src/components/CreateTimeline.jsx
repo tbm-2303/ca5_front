2a7e0f256df724b4d183e198b45d8b9d8d100a8c
Annika Rosenvinge Jespersen <Annika2000@live.dk>
2022-05-19 14:23:21 +0200

    create timeline only style

--- src/components/CreateTimeline.jsx
+++ src/components/CreateTimeline.jsx
@@ -1,11 +1,50 @@
 import React from "react";
+import {timelineCreate} from "../settings";
+//husk tilbage knapper
+const CreateTimeline = () => {
 
-const CreateTimeline = ({props}) => {
+    const createTimelineURL = async (timeline) => {
+        const res = await fetch(timelineCreate, {
+            method: "POST",
+            headers:{
+                "Content-type": "application/json",
+            },
+            body: JSON.stringify(timeline)
+        });
+    }
+
+    function createTimeline(){
+        const name = document.getElementById("timelineName").value;
+        const description = document.getElementById("timelineDes").value;
+        const startDate = document.getElementById("timelineStart").value;
+        const endDate = document.getElementById("timelineEnd").value;
+
+        const timeline ={
+            "timelineName" : name,
+            "timelineDes": description,
+            "timelineStart" : startDate,
+            "timelineEnd": endDate
+        }
+        console.log(JSON.stringify(timeline));
+    }
     return (
         <div className="tableTimeline">
             <h2>Her kan du lave en ny timeline</h2>
+            <h4>Navn</h4>
+            <input type="text" id="timelineName"/>
+            <br/>
+            <h4>Beskrivelse</h4>
+            <input type="text" id="timelineDes"/>
+            <br/>
+            <h4>Start tidspunkt</h4>
+            <input type="text" id="timelineStart"/>
+            <br/>
+            <h4>Slut tidspunkt</h4>
+            <input type="text" id="timelineEnd"/>
+            <br/>
         </div>
     );
 }
+
 
 export default CreateTimeline;
